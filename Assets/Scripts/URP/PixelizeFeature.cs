631c2312ea0808f111815b6ef568d61c2b597e11
willake <willakelin@gmail.com>
2023-09-27 23:46:22 +0200

    update: add pixelate render feature

---
+++ Assets/Scripts/URP/PixelizeFeature.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace Game.URP
+{
+    public class PixelizeFeature : ScriptableRendererFeature
+    {
+        [System.Serializable]
+        public class CustomPassSettings
+        {
+            public RenderPassEvent renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+            public int screenHeight = 144;
+        }
+
+        [SerializeField] private CustomPassSettings _settings = new();
+        private PixelizePass _customPass;
+
+        public override void Create()
+        {
+            _customPass = new PixelizePass(_settings);
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+#if UNITY_EDITOR
+            if (renderingData.cameraData.isSceneViewCamera) return;
+#endif
+            renderer.EnqueuePass(_customPass);
+        }
+    }
+}
