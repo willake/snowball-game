dea7645f682c36f31fda6ba0885f0ef03441828b
Hui En Lin <huienlin.game@gmail.com>
2023-10-17 21:54:11 +0200

    update: modify player in view logic

--- Assets/Scripts/Gameplay/CustomBehaviorTree/Nodes/DetectPlayerService.cs
+++ Assets/Scripts/Gameplay/CustomBehaviorTree/Nodes/DetectPlayerService.cs
@@ -10,6 +10,8 @@ namespace Game.Gameplay.CustomBehaviorTree
     [MBTNode("Custom Services/Detect Player Service")]
     public class DetectPlayerService : Service
     {
+        public LayerMask enemyLayer;
+        public LayerMask playerLayer;
         public Vector3State statePlayerPos;
         [Tooltip("Sphere radius")]
         public float sightRange = 10;
@@ -21,15 +23,28 @@ namespace Game.Gameplay.CustomBehaviorTree
             // Find target in radius and feed blackboard variable with results
             float distance = Vector3.Distance(transform.position, statePlayerPos.value);
 
-            if (distance < sightRange)
+            RaycastHit hit;
+            bool canSeePlayer = false;
+            if (Physics.Raycast(
+                transform.position, statePlayerPos.value - transform.position,
+                out hit, Mathf.Infinity, ~enemyLayer))
+            {
+                if (1 << hit.collider.gameObject.layer == playerLayer)
+                {
+                    canSeePlayer = true;
+                }
+            }
+
+            if (canSeePlayer && distance < sightRange)
             {
                 isPlayerInView.Value = true;
-                playerPos.Value = statePlayerPos.value;
             }
             else
             {
                 isPlayerInView.Value = false;
             }
+
+            playerPos.Value = statePlayerPos.value;
         }
 
         public override bool IsValid()
